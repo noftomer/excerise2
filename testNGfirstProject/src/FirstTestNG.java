176e863bcad62955fefac71f2930f4ba8d1d5138
tomer <tomnof81@gmail.com>
2018-11-30 17:46:23 +0200

    test

---
+++ testNGfirstProject/src/FirstTestNG.java
@@ -0,0 +1,7 @@
+import org.testng.annotations.Test;
+
+public class FirstTestNG {
+  @Test
+  public void f() {
+  }
+}
---
+++ testNGfirstProject/src/TestNGClass.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class TestNGClass {
+	WebDriver driver;
+  @BeforeClass (groups= {"Sanity"})
+  public void start() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver_win32\\chromedriver_original.exe");
+	  driver=new ChromeDriver();
+	  
+  }
+  @AfterClass (groups= {"Sanity"})
+  public void close() {
+	  try {
+		Thread.sleep(1500);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  driver.quit();
+  }
+	@Test(groups= {"Sanity"})
+  public void test1() {
+	  driver.get("https://www.google.co.il/?hl=iw");
+	  System.out.println("In "+Thread.currentThread().getStackTrace()[1].getMethodName()+": thread="+Thread.currentThread().getId());
+	  
+  }
+	@Test(groups= {"Sanity"})
+	  public void test2() {
+		  driver.get("https://www.google.co.il/?hl=iw");
+		  System.out.println("In "+Thread.currentThread().getStackTrace()[1].getMethodName()+": thread="+Thread.currentThread().getId());
+		  
+	  }
+	@Test
+	  public void test3() {
+		  driver.get("https://www.google.co.il/?hl=iw");
+		  System.out.println("In "+Thread.currentThread().getStackTrace()[1].getMethodName()+": thread="+Thread.currentThread().getId());
+	  }
+
+	@Test
+	  public void test4() {
+	  driver.get("https://www.google.co.il/?hl=iw");
+		  System.out.println("In "+Thread.currentThread().getStackTrace()[1].getMethodName()+": thread="+Thread.currentThread().getId());
+	  }
+
+	@Test(groups= {"Sanity"})
+	  public void test5() {
+	  driver.get("https://www.google.co.il/?hl=iw");
+		  System.out.println("In "+Thread.currentThread().getStackTrace()[1].getMethodName()+": thread="+Thread.currentThread().getId());
+	  }
+}
